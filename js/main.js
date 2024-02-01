1fabee42d4d66c98bb6eab582a322a15fb68a6ca
DESKTOP-RLIRLHO\user <>
2024-02-01 12:44:09 +0900

    feat : tablet 버전 footer 전체 수정, main page 수정

--- js/main.js
+++ js/main.js
@@ -1,13 +1,11 @@
 $(function(){
   // 비주얼 슬라이드
   let currentIndex = 0;
-
   setInterval(function(){
       let nextIndex = (currentIndex + 1) % 3;
       $(".slider").eq(currentIndex).fadeOut(1200);
       $(".slider").eq(nextIndex).fadeIn(1200);
       currentIndex = nextIndex;
-
   },3000)
 
 //    let classbtn = $(".classbtn > li");
@@ -20,18 +18,8 @@ $(function(){
 //     $(this).addClass("active").siblings().removeClass("active");
 //     classList.eq(index).show().siblings().hide();
 //    })
-// 강좌 탭
-  $('.classbtn > li').on('click', function(){
-      var classBtn = $(this);
-      var idx = classBtn.index();
-      var box = classBtn.closest('#class');
-      box.find('.tab_content').removeClass('on');
-      box.find('.classbtn > li').removeClass('on');
-      classBtn.addClass('on');
-      box.find('.tab_content:eq('+idx+')').addClass('on');
-      return false
-  })
 
+// information
   $("#information ul li").mouseover(function(){
     var element = $(this).find("a > span");
 element.css("display", "block");
@@ -40,6 +28,56 @@ element.css("display", "block");
     var element = $(this).find("a > span");
     element.css("display", "none");
   })
+
+// 강좌 탭
+$(document).ready(function() {
+    // 화면 크기 변경 이벤트에 대한 핸들러
+    function adjustTabsVisibility() {
+        // 현재 창의 너비를 가져오기
+        var windowWidth = $(window).width();
+
+        // 각 .tab_content에 대해 처리
+        $('#class .tab_content .classlist').each(function() {
+            var tabContent = $(this);
+
+            // 창 너비가 768px 미만인 경우
+            if (windowWidth <= 768) {
+                // 각 .tab_content 내의 li 중 처음 3개만 선택하여 나머지는 숨김
+                tabContent.find('>li:lt(3)').show();
+                tabContent.find('>li:gt(2)').hide();
+            } else {
+                // 창 너비가 768px 이상인 경우 모든 li를 보이도록 함
+                tabContent.find('li').show();
+            }
+        });
+    }
+
+    // 초기 로드 시 한 번 실행하여 초기 화면에 적용
+    adjustTabsVisibility();
+
+    // 화면 크기 변경 시 이벤트 핸들러 등록
+    $(window).on('resize', adjustTabsVisibility);
+
+    // 클릭 이벤트 핸들러
+    $('.classbtn > li').on('click', function() {
+        var classBtn = $(this);
+        var idx = classBtn.index();
+        var box = classBtn.closest('#class');
+        
+        // 다른 부분은 기존 코드와 동일
+        box.find('.tab_content').removeClass('on');
+        box.find('.classbtn > li').removeClass('on');
+        classBtn.addClass('on');
+        box.find('.tab_content:eq('+idx+')').addClass('on');
+        
+        // 창 크기에 따라 다시 적용
+        adjustTabsVisibility();
+        
+        return false;
+    });
+});
+
+  
   // 갤러리
   var swiper = new Swiper(".mySwiper", {
       slidesPerView: 4,
